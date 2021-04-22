c2c521304431842f0ea919c0faa88884665d15ef
WhiteDragon89 <bt0804@naver.com>
2021-04-23 00:00:16 +0900

    test

---
+++ src/main/java/com/example/start/CONTROLLER/HELLOCONTROLLER.java
@@ -0,0 +1,35 @@
+package com.example.start.CONTROLLER;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HELLOCONTROLLER {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!!");
+                return "hello";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello ttApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private  String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
